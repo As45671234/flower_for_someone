21842fdf55e1bb9e819baab357f8a7fcee15a01e
As45671234 <83564736+As45671234@users.noreply.github.com>
2025-01-03 01:26:48 +0500

    Add files via upload

---
+++ enter/js.js
@@ -0,0 +1,16 @@
+window.onload = () => {
+    const submitButton = document.getElementById('submit-btn');
+    const usernameInput = document.getElementById('username');
+    const errorMessage = document.getElementById('error-message');
+  
+    submitButton.addEventListener('click', () => {
+      const username = usernameInput.value.trim().toLowerCase();
+        
+      if (username === 'nargiz' || username === 'наргиз' || username === 'nako' || username === 'нако') {
+        window.location.href = `../index.html`;
+      } else {
+        errorMessage.textContent = 'Это не для тебя!';
+      }
+    });
+  };
+  
